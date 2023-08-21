9913d18e37ecfb001be410a434c25cc043ea7910
DIMITRI ZENARO BASTOS DE OLIVEIRA <22.00820-9@maua.br>
2023-08-21 09:17:19 -0300

    ap03: exercicio CRUD de contato

--- bin/colecoes.dart
+++ bin/colecoes.dart
@@ -1,19 +1,167 @@
 import 'dart:math';
 import 'dart:io';
-void exercicio2(){
-  List <int> listaEntradas = [];
-  while (listaEntradas.length<6) {
-    print("Digite um número: ");
-    var entrada = stdin.readLineSync()!;
-    var numero = int.parse(entrada);
-    if(!listaEntradas.contains(numero)){
-      listaEntradas.add(numero);
-    }
-    else {
-      print("Número Duplicado");
-  }}
+void exercicio1(){
+  int op;
+  Map<String, String> contatos = {
+    'Ana' : '12345678'
 
-  print(listaEntradas);
+  };
+  do{
+    print("1-C\n2-R\n3-U\n4-D\n5-Sair")
+    op = int.parse(stdin.readLineSync()!);
+    switch(op){
+      case 1:
+        print("Criar contato: ")
+        print("Digite o nome do NOVO contato: ");
+        nome = stdin.readLineSync()!;
+        print("Digite o telefone do NOVO contato: ");
+        telefone = stdin.readLineSync()!;
+        contatos[nome] = telefone;
+        break;
+      
+      case 2:
+        print("Consulta de contato: ")
+        var input = stdin.readLineSync()!;
+        if (contatos[input] != null){
+          print("Nome: $input\nNumero: ${contatos[input]}");
+        }
+        else{
+          print("Contato não encontrado");
+        }
+        break;
+
+      case 3:
+        print("Atualização de contato: ")
+        print("Digite o nome do contato: ");
+        input = stdin.readLineSync()!;
+        
+        if (contatos.containsKey(input)){
+          print("Digite novo número: ")
+          numero = stdin.readLineSync();
+          contatos[input] = numero;
+          print("Contato atualizado!!\n$input\n$numero")
+        }
+        else{
+          print("Contato não existente")
+        }
+        
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      default:
+        print("Opção inválida");
+    }
+  }
+}
+
+// void main(){
+  
+//   var pessoa = {
+//     'nome':'Ana',
+//     'idade':19
+//   };
+
+//   //values
+//   var valores = pessoa.values;
+//   for(final valor in valores){
+//     print(valor);
+//   }
+  
+  
+//   //entries
+//   var entries = pessoa.entries;
+//   for(final entry in entries){
+//     print(entry);
+//     print(entry.key);
+//     print(entry.value);
+//   }
+  
+  
+  
+  
+  
+  
+  
+  
+  //keys
+  // var keys = pessoa.keys;
+  // print(keys);
+  // print(keys.runtimeType);
+  // for (final key in  keys){
+  //   print(key);
+  //   print(pessoa[key]);
+  // }
+  // var nome = pessoa['nome'] as String;
+
+  // print(pessoa['nome']);
+  // print(pessoa['altura']);
+  
+  
+  //<String, Object>
+  // var pessoa = <String, Object>{
+  //   'nome':'Ana',
+  //   'idade':19
+  // }
+  
+  
+  // Map <String, Object> pessoa ={
+  //   'nome': 'Ana',
+  //   'idade': 25,
+  //   'altura': 1.8,
+  //   'solteira': true,
+  //   'amigos': ['João', 'Pedro', 'Maria']
+  // };
+  
+  
+  
+  
+  //não é erro em compilação ou execução
+  // var pessoa = {
+  //   'nome' : 'Ana',
+  //   'nome' : 'Pedro'
+  // };
+  
+  
+  
+  //mapa são coleções de pares chaves/valor
+  // var pessoa = {
+  //   'nome' : 'Ana',
+  //   'altura' : 1.8
+  // };
+
+  // var lembretes = {
+  //   1: 'comprar café',
+  //   2: 'ver um filme',
+  // };
+  
+  // print(pessoa.runtimeType)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // List <int> listaEntradas = [];
+  // while (listaEntradas.length<6) {
+  //   print("Digite um número: ");
+  //   var entrada = stdin.readLineSync()!;
+  //   var numero = int.parse(entrada);
+  //   if(!listaEntradas.contains(numero)){
+  //     listaEntradas.add(numero);
+  //   }
+  //   else {
+  //     print("Número Duplicado");
+  // }}
+
+  // print(listaEntradas);
   
   /*
   Exercício. Escreva um programa que:
